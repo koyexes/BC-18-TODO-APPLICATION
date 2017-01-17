2546db55de5da093d297f17480ce3a0eb1918582
koyexes <no-reply@email.com>
2017-01-17 01:15:51 +0100

    Creates Add/Delete Board and List logic

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
  * Created by koyexes on 1/16/2017.
  */
 var {firebase} = require('./config');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
 var uid;
 firebase.auth().signInWithEmailAndPassword("koyagabriel@gmail.com", "Sunday_1009").then((value) => {
     uid = value.uid;
@@ -9,12 +15,43 @@ firebase.auth().signInWithEmailAndPassword("koyagabriel@gmail.com", "Sunday_1009
     console.log(error);
 });
 var database = firebase.database();
-// database.ref('lists').push({
-//     title: "Monday Activities",
-//     author: "Koya Gabriel",
-//
-// });
-database.ref('lists/-Kad38-UDqDIhvW9Vghj/card').push({
-    title: "noon prayers",
-    Time:  "9:30"
+var ref = database.ref('boards/');
+
+app.post('/board', (request,response) => {
+    var title = request.body.title;
+    ref.push({
+        title,
+    }).then((value) => response.send(value.key)).catch((error) => console.log(error));
+});
+
+app.delete('/board/:key', (request, response) => {
+    var key = request.params.key;
+    ref.child(key.toString()).remove().then((value) => {
+        response.send(`Successfully deleted`);
+    }).catch((error) => console.error(error));
+});
+
+app.post('/list', (request, response) => {
+    var boardKey = request.body.key;
+    var title = request.body.title;
+    var cards = "empty";
+    var listRef = ref.child(boardKey.toString() + '/list');
+    listRef.push({
+        title,
+    }).then((value) => response.send(value.key)).catch((error) => console.log(error));
+});
+
+app.delete('/list/:key', (request, response) => {
+    var boardKey = request.body.key;
+    var listKey = request.params.key;
+    ref.child(boardKey.toString() + '/list/' + listKey.toString()).remove().then((value) => {
+        response.send(`Successfully deleted`);
+    }).catch((error) => console.error(error));
+});
+
+app.post('/card')
+
+
+app.listen(3000, () => {
+   console.log('Started on port 3000');
 });
